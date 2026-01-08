9015c57f1293d64bc37e6634b0dcc32a17a8602a
kkarshieva <kamilakarshieva@gmail.com>
2026-01-08 20:03:50 +0000

    initial placeholder pages

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def root():
     return render_template("base.html")
+
+
+@app.route("/onboarding", methods=["GET"])
+def onboarding():
+    return render_template("onboarding.html")
+
+
+@app.route("/matching", methods=["GET"])
+def matching():
+    return render_template("matching.html")
+
+
+@app.route("/dashboard", methods=["GET"])
+def dashboard():
+    return render_template("dashboard.html")
